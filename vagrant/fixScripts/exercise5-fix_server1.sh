9425f43aceb7b911d4d1bd60db827ce20eec3f17
itaiv11 <45734448+itaiv11@users.noreply.github.com>
2019-08-18 20:30:31 +0300

    Update exercise5-fix_server1.sh

--- vagrant/fixScripts/exercise5-fix_server1.sh
+++ vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 #add fix to exercise5-server1 here
+ssh-keygen -t rsa
+key-123456
+passpharse-123456
+123456
+
+ssh server mkdir -p .ssh
+vagrant
+
+cat .ssh/id_rsa .pub | ssh server2 'cat >> .ssh/authorized_keys'
+server2 password- vagrant 
+
+done
