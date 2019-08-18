6f9c8befde8af4983a6a4010f930d9802da5be2e
itaiv11 <45734448+itaiv11@users.noreply.github.com>
2019-08-18 19:02:35 +0300

    Update exercise2-fix.sh
    007 bond, james bond (:

--- vagrant/fixScripts/exercise2-fix.sh
+++ vagrant/fixScripts/exercise2-fix.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 #add fix to exercise2 here
+ping -a www.ascii-art.de(check ip)
+sudo nano /etc/hosts
+change 127.0.0.1 (localhost) to correct ip
+curl http://www.ascii-art.de/ascii/ab/007.txt
+
+saw 007 
