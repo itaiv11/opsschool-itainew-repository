83c9c28d953de4e4a2cfd3937ab5f6a8101bcc18
itaiv11 <45734448+itaiv11@users.noreply.github.com>
2019-08-19 16:28:20 +0300

    Update exercise2-fix.sh
    ping -a www.ascii-art.de(check ip)
    enter to hosts and see
    change 127.0.0.1 (localhost) to correct ip
    
    saw 007

--- vagrant/fixScripts/exercise2-fix.sh
+++ vagrant/fixScripts/exercise2-fix.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 #add fix to exercise2 here
-ping -a www.ascii-art.de(check ip)
 sudo nano /etc/hosts
-change 127.0.0.1 (localhost) to correct ip
+5.35.226.130 /www.ascii-art.de
+ctr+x
+y
+
 curl http://www.ascii-art.de/ascii/ab/007.txt
 
-saw 007 
+
